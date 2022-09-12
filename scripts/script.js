78f9dde387a28da8e7211869035312b4cd2c5d0d
Jaafar Hawli <jaafarhawle@gmail.com>
2022-09-12 23:59:16 +0300

    added some constraints to contact info

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
 let login = document.getElementById('show-window');
 let logout = document.getElementById('close-window');
 let exit = document.getElementById('exit');
+let fname = document.getElementById('name').value;
+let email = document.getElementById('email').value;
+let number = document.getElementById('number').value;
+let text = document.getElementById('text').value;
+let alert1 = document.getElementById('alert');
+let number_exp = '^[+961][0-9]{8}$';
 
 login.addEventListener('click', () => {
 	document.querySelector('.pop-up').classList.add('active');
@@ -13,3 +19,16 @@ logout.addEventListener('click', () => {
 exit.addEventListener('click', () => {
 	document.querySelector('.pop-up').classList.remove('active');
 });
+
+if (fname.length < 5) {
+	alert1.innerHTML = 'Full name has less than 5 characters';
+}
+
+if (text.length < 100) {
+	alert1.innerHTML = 'Message has less than 100 characters';
+}
+if (number.match(number_exp)) {
+	alert1.innerHTML += '';
+} else {
+	alert1.innerHTML += 'Wrong number';
+}
