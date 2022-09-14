2891335c78898c7cf8f328b16379d29e0279024e
JaafarHawli <jaafarhawle@gmail.com>
2022-09-14 12:15:45 +0300

    added a table for users

--- scripts/db.js
+++ scripts/db.js
@@ -1,4 +1,5 @@
 const myform = document.getElementById('form_id');
+const contact_list = document.getElementById('contact_list');
 
 myform.addEventListener('submit', (e) => {
 	e.preventDefault();
@@ -24,6 +25,16 @@ fetch('http://localhost/freelancer-clone-backend/freelancer-clone-backend/view_u
 	.then((Response) => Response.json())
 	.then((ResponseJson) => {
 		for (const key in ResponseJson) {
-			console.log(ResponseJson[key]);
+			/* console.log(ResponseJson[key]); */
+			let table = document.getElementById('table');
+			let row = table.insertRow(0);
+			let cell1 = row.insertCell(0);
+			let cell2 = row.insertCell(1);
+			let cell3 = row.insertCell(2);
+			let cell4 = row.insertCell(3);
+			cell1.innerHTML = ResponseJson[key].full_name;
+			cell2.innerHTML = ResponseJson[key].email_address;
+			cell3.innerHTML = ResponseJson[key].phone_number;
+			cell4.innerHTML = ResponseJson[key].message;
 		}
 	});
